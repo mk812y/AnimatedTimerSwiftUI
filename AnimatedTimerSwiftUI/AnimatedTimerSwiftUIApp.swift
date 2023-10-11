69ca9fb092ced8cb503c7eb711825755c06f8c7d
mk812y <mk812y@gmail.com>
2023-10-11 21:10:37 +0200

    Initial Commit

---
+++ AnimatedTimerSwiftUI/AnimatedTimerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTimerSwiftUIApp.swift
+//  AnimatedTimerSwiftUI
+//
+//  Created by Михаил Куприянов on 11.10.23..
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTimerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
